5efea3c4a21beffbada75d897f283865ac4e1f9d
Jason Kralik <jgkra123@gmail.com>
2018-06-12 23:21:55 -0400

    Auto-Screencapture

--- 51-Screencapture.sh
+++ 51-Screencapture.sh
@@ -5,42 +5,42 @@
 # Author: Jason K
 # Purpose: ScreenCapture Prototype
 # FileName: 51-Screencapture.sh
-# Created: 06/08/18 
-# Modifed:                                  
+# Created: 06/08/2018 
+# Modifed: 06/12/2018
 #                                           
 ############################################
 
-dirPath=~/Pictures/51-Screenshots/
 
-# Time Variables Set
-minute=$(date +%M)
-hour=$(date +%H)
-date=$(date +%d)
-month=$(date +%m)
-year=$(date +%Y)
-
+while true
+do
+	dirPath=~/Pictures/51-Screenshots/
+	minute=$(date +%M)
+	hour=$(date +%H)
+	date=$(date +%d)
+	month=$(date +%m)
+	year=$(date +%Y)
 # Formatting for Directories & Files based on dates & times
-monthDirPath=$dirPath$year-$month
-currentDateTime=$year-$month-$date-$hour$minute
+	monthDirPath=$dirPath$year-$month
+	fileName=$(date +%Y-%m-%d-%H%M)
 
-function DirMain() {
-	# Creates main directory called '51-Screenshots' if it does not exist
+# Creates main directory called '51-Screenshots' if it does not exist
+function dirMain() {
 	if [ ! -d $dirPath ]
 		then
 			mkdir $dirPath
 	fi
 }
 
-function DirMonth() {
-	# Creates subDirectory based upon the month if it does not exist
-	if [ ! -d $monthDirPath/ ]
+# Creates subDirectory based upon the month if it does not exist
+function dirMonth() {
+	if [ ! -d $monthDirPath ]
 		then
-			mkdir $monthDirPath/
+			mkdir $monthDirPath
 	fi
 }
 
-function DirPayPeriod() {
-	# Creates subDirectory based upon pay period if it does not exist
+# Creates subDirectory based upon pay period if it does not exist
+function dirPayPeriod() {
 	if [ $date -lt 16 ]
 		then
 			subDirPath=$monthDirPath/1-15
@@ -55,32 +55,58 @@ function DirPayPeriod() {
 					mkdir $subDirPath
 			fi
 	fi
+	# Creating whole path to the file in one variable
+	fullPathToFile=$subDirPath/$fileName
 }
 
-DirMain
-DirMonth
-DirPayPeriod
+# Screencapture time controller
+function shotCaller() {
+	if [ $minute == $2 ]
+		then
+			screencapture $1
+	fi
+}
+#############
+# NOTE: shotcaller() should be changed to gui controlled interface for 
+#  starting & stopping of screencaptures & customizations when rewritten 
+#  in python or swift
+#############
 
-# Creating whole path to the file in one variable
-fullPathToFile=$subDirPath/$currentDateTime
+# Calling functions
+	dirMain
+	dirMonth
+	dirPayPeriod
+	shotCaller $fullPathToFile $1 
 
-# Instead I could just automate it and create a name and place each time
-screencapture $fullPathToFile
-
-# Send screenshots to correct directory using special name 
-#		& then reset screenshot location & name
-#		automate screencapture while 51-talk app is on at 5, 20, 35 & 50 minutes after hour
-
+##############
 # gzip
 #		automate gzip of directories 
 #		provide ungzip functionality 
+'''
+GZIP-->
+=$
+zipTime=$month+$
+if [ $zipTime == 
 
+'''
+	sleep 60
+done
 
-
+###############
 # This is here for reference 
-#--->	default write com.apple.screencapture location $subDirPath name $currentDateTime
+#--->	default write com.apple.screencapture location $subDirPath name $currentDateTime type $JpgTiffGifPdfPng
 #---> killall SystemUIServer
+#---> default delete com.apple.screencapture location
 
-
-
+################
+#---> default location script prototype...
+# read -p "Would you like to restore screencapture location to default? (y)" restore
+# if [ restore == "y" ]
+#		then
+#			defaults write com.apple.screencapture location ~/Desktop/
+#			killall SystemUIServer
+#			echo "Screencapture location returned to Desktop."
+#		else
+#			echo "Screencapture location unchanged."
+#	fi
 
